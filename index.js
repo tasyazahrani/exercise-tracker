85b3b6b8a309e089d4233c6fc1f0a0a0d3678bbd
Tasya Zahrani <tasyaa23@mhs.usk.ac.id>
2025-05-23 14:28:52 +0700

    first commit

--- index.js
+++ index.js
@@ -96,6 +96,6 @@ function generateId() {
 
 
 
-const listener = app.listen(process.env.PORT || 3001, () => {
+const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
 })
